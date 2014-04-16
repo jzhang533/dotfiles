3e303d0c7068aa0f31d6357d4ca5330b470bd197
Zhang Jun <jzhang533@gmail.com>
2014-04-16 14:11:15 +0800

    added git config

---
+++ git-config.sh
@@ -0,0 +1,11 @@
+git config --global user.name "Zhang Jun"
+
+git config --global color.branch "auto"
+git config --global color.ui "auto"
+git config --global color.status "auto"
+
+git config --global alias.st "status"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+
+git config --global core.editor "vim"
+
